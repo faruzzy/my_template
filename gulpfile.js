5bf0a67b81b9ed023cbe47cc662574215a7c62e5
Roland Pangu <faruzzy@gmail.com>
2016-07-02 02:10:23 -0700

    ES6 baby

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
 
-gulp.task('serve', function() {
+gulp.task('serve', () => {
     browserSync.init({
         server: 'app/'
     });
 });
 
-gulp.task('default', ['serve'], function() {
+gulp.task('default', ['serve'], () => {
     gulp.watch('app/**/*.html').on('change', browserSync.reload);
     gulp.watch('app/scripts/**/*.js').on('change', browserSync.reload);
 });
