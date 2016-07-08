5b863e733641bc4474407b4649325df8a1e0eb9c
Roland Pangu <faruzzy@gmail.com>
2016-07-07 20:57:47 -0700

    browserify photo bombed the picture

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-const gulp = require('gulp'),
+/*const gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     babel = require('gulp-babel'),
-	sass = requre('gulp-sass');
+	sass = requre('gulp-sass');*/
+
+const gulp = require('gulp'),
+    babelify = require('babelify'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    browserSync = require('browser-sync').create(),
+    sass = requre('gulp-sass');
 
 gulp.task('sass', () => {
 	return gulp.src('app/scss/*.scss')
-		.pipe(autoprefixer())
+		//.pipe(autoprefixer())
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(gulp.dest('app/css/*.css'))
 		.pipe(browserSync.stream());
@@ -17,17 +25,27 @@ gulp.task('serve', () => {
     });
 });
 
+// gulp.task('es6', () => {
+//     return gulp.src('app/js/**/*.js')
+//         .pipe(babel({
+//             presets: ['es2015']
+//         }))
+//         .pipe(gulp.dest('app/build'));
+// });
+
 gulp.task('es6', () => {
-    return gulp.src('app/js/**/*.js')
-        .pipe(babel({
+    browserify('src/app.js')
+        .transform('babelify', {
             presets: ['es2015']
-        }))
-        .pipe(gulp.dest('app/build'));
+        })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('app/build/'));
 });
 
 gulp.task('default', ['serve', 'es6', 'sass'], () => {
     gulp.watch('app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('app/scripts/**/*.js').on('change', browserSync.reload);
-    gulp.watch('app/scripts/**/*.js');
+    gulp.watch('app/js/**/*.js', ['es6']).on('change', browserSync.reload);
 	gulp.watch('app/scss/*.scss', ['sass']);
 });
